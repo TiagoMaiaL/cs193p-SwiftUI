d317d3338fe0085ffceb5aeff017462eb92d4116
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2021-06-06 20:55:18 -0300

    Add the initial project for assignment 1

---
+++ assignment-1/MemorizeUI/MemorizeUI/Application/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  MemorizeUI
+//
+//  Created by Tiago Lopes on 06/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ assignment-1/MemorizeUI/MemorizeUI/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemorizeUI
+//
+//  Created by Tiago Lopes on 06/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
