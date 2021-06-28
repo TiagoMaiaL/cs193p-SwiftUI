d8e1450871fc43f9a637f2b34534d8ab02278133
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2021-06-28 19:38:39 -0300

    Assignment-2: Add initial Xcode project

---
+++ assignment-2/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Tiago Lopes on 28/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
