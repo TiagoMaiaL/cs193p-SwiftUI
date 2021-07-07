619315633e47877c81c67468c219c8024f3c6143
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2021-07-07 19:35:10 -0300

    Assignment-2: Start adding support for themes

---
+++ assignment-2/Memorize/Memorize/Models/Theme.swift
@@ -0,0 +1,22 @@
+//
+//  Theme.swift
+//  Memorize
+//
+//  Created by Tiago Lopes on 07/07/21.
+//
+
+import Foundation
+
+struct Theme<Content> where Content: Equatable {
+    
+    // MARK: Properties
+    
+    let name: String
+    let pairsCount: Int
+    let options: [Content]
+    let color: Color
+    // TODO: Add and test the shuffled options.
+}
+
+// TODO: Make a Memorize extension that accepts a Theme.
+// TODO: Add a namespace for Emoji themes.
---
+++ assignment-2/Memorize/Memorize/Models/ThemeColor.swift
@@ -0,0 +1,19 @@
+//
+//  ThemeColor.swift
+//  Memorize
+//
+//  Created by Tiago Lopes on 07/07/21.
+//
+
+import Foundation
+
+extension Theme {
+    enum Color {
+        case red
+        case green
+        case blue
+        case purple
+        case pink
+        case gray
+    }
+}
