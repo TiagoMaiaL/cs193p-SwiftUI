5fcdefae3485a98e55cda1d02a40c66611969b76
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2021-09-05 13:55:25 -0300

    Assignment-2: Refactor the EmojiTheme code

--- assignment-2/Memorize/Memorize/Model/Emoji.swift
+++ assignment-2/Memorize/Memorize/Model/Emoji.swift
@@ -9,64 +9,65 @@ import Foundation
 
 typealias Emoji = String
 typealias EmojiMemorize = Memorize<Emoji>
+typealias EmojiTheme = Theme<Emoji>
 
 enum EmojiThemes {
     
     // MARK: Themes
     
-    static let nature = Theme(
+    static let nature = EmojiTheme(
         name: "Nature",
         pairsCount: 6,
         options: ["🌳", "🌲", "🌵", "🌴", "⛰", "🗻"],
         color: .green
     )
     
-    static let flags = Theme(
+    static let flags = EmojiTheme(
         name: "Flags",
         pairsCount: 10,
         options: ["🇧🇷", "🇪🇸", "🏴󠁧󠁢󠁥󠁮󠁧󠁿", "🇺🇸", "🇿🇦", "🇲🇽", "🇯🇵", "🇵🇹", "🇩🇪", "🇵🇱", "🇦🇹", "🇮🇹", "🇫🇷", "🇦🇷", "🇨🇱"],
         color: .gray
     )
     
-    static let faces = Theme(
+    static let faces = EmojiTheme(
         name: "Faces",
         pairsCount: 6,
         options: ["😀", "😃", "😂", "🥲", "😎", "🧐"],
         color: .yellow
     )
     
-    static let halloween = Theme(
+    static let halloween = EmojiTheme(
         name: "Halloween",
         pairsCount: 6,
         options: ["🎃", "👻", "💀", "🦇", "🌕", "🐺"],
         color: .purple
     )
     
-    static let vehicles = Theme(
+    static let vehicles = EmojiTheme(
         name: "Vehicles",
         pairsCount: 7,
         options: ["🚔", "🚗", "🚒", "🚚", "🚌", "🛻", "🚛", "🚑", "🚜"],
         color: .blue
     )
     
-    static let food = Theme(
+    static let food = EmojiTheme(
         name: "Food",
         pairsCount: 6,
         options: ["🌭", "🌶", "🍔", "🍟", "🍏", "🍇"],
         color: .red
     )
     
-    static var allThemes: [Theme<Emoji>] {
+    static var allThemes: [EmojiTheme] {
         [nature, flags, faces, halloween, vehicles, food]
     }
     
     // MARK: Random
     
-    static var random: Theme<Emoji> {
+    static var random: EmojiTheme {
         allThemes.randomElement()!
     }
     
-    static func random(excluding themeToExclude: Theme<Emoji>) -> Theme<Emoji> {
+    static func random(excluding themeToExclude: EmojiTheme) -> EmojiTheme {
         var themes = allThemes
         
         guard let removalIndex = allThemes.firstIndex(where: { $0.name == themeToExclude.name }) else {
