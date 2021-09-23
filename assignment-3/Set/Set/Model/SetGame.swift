3b33a6074ccd2c759d8f1c89ff8a5bdc17cc143b
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2021-09-23 19:11:37 -0300

    Assignment-3: include AspectVGrid

---
+++ assignment-3/Set/Set/Model/SetGame.swift
@@ -0,0 +1,26 @@
+//
+//  SetGame.swift
+//  Set
+//
+//  Created by Tiago Lopes on 23/09/21.
+//
+
+import Foundation
+
+struct SetGame {
+    // TODO: Add an array representing the table.
+    // TODO: Add a Set representing the deck.
+    // TODO: Add a Set representing the matched cards.
+    // TODO: Add a method to deal more cards.
+    // TODO: Add a method to choose a card.
+    // TODO: Add a method to check for a match.
+    // TODO: Add a method to start a new game.
+    // TODO: Add a property to check if the game is finished.
+    
+    // TODO: Add the Card struct.
+    // TODO: Add the Feature enums.
+    
+    // TODO: Add a DeckFactory.
+    
+    // TODO: Do we need to make the contents of the card generic?
+}
