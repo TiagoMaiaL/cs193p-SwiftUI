07b01cdeb4a5e680243e756c6cbbbdff14f9f2f8
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2021-09-23 09:29:31 -0300

    Assignment-3: Add the initial xcode project for the Set game

---
+++ assignment-3/Set/Set/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  Set
+//
+//  Created by Tiago Lopes on 23/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ assignment-3/Set/Set/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set
+//
+//  Created by Tiago Lopes on 23/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
