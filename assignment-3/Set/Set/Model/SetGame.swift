c2d1e7d6500f167d912aafe135584002a41c6024
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2021-09-24 21:28:22 -0300

    Assignment-3: Add the initial properties to SetGame

--- assignment-3/Set/Set/Model/SetGame.swift
+++ assignment-3/Set/Set/Model/SetGame.swift
@@ -8,19 +8,20 @@
 import Foundation
 
 struct SetGame {
-    // TODO: Add an array representing the table.
-    // TODO: Add a Set representing the deck.
-    // TODO: Add a Set representing the matched cards.
+    
+    // MARK: Properties
+    
+    private var deck: Set<Card>
+    private(set) var table = [Card]()
+    private var matchedCards = Set<Card>()
+    
     // TODO: Add a method to deal more cards.
     // TODO: Add a method to choose a card.
     // TODO: Add a method to check for a match.
     // TODO: Add a method to start a new game.
     // TODO: Add a property to check if the game is finished.
-    
-    // TODO: Add the Card struct.
-    // TODO: Add the Feature enums.
-    
+        
     // TODO: Add a DeckFactory.
     
-    // TODO: Do we need to make the contents of the card generic?
+    // TODO: Decide if we need to make the cards content generic.
 }
