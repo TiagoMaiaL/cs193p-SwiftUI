588c73a6723a1c21f993f39d2c4221bfb11b9ffd
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2021-10-11 09:14:05 -0300

    Assignment-3: Add empty SetGameView

---
+++ assignment-3/Set/Set/View/SetGameView.swift
@@ -0,0 +1,29 @@
+//
+//  SetGameView.swift
+//  Set
+//
+//  Created by Tiago Lopes on 08/10/21.
+//
+
+import SwiftUI
+
+// MARK: - View
+
+struct SetGameView: View {
+    
+    // MARK: Properties
+    
+    // MARK: Body
+    
+    var body: some View {
+        EmptyView()
+    }
+}
+
+// MARK: - Preview
+
+struct SetGameView_Previews: PreviewProvider {
+    static var previews: some View {
+        SetGameView()
+    }
+}
