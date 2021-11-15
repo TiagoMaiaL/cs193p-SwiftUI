0f9c27be849845607932b4e5312e273496ec175f
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2021-11-15 15:12:40 -0300

    Assignment-3: Add the squiggle-shape view

---
+++ assignment-3/Set/Set/View/SetCardView/SquiggleView.swift
@@ -0,0 +1,21 @@
+//
+//  SquiggleView.swift
+//  Set
+//
+//  Created by Tiago Lopes on 15/11/21.
+//
+
+import SwiftUI
+
+struct SquiggleView: View {
+    var body: some View {
+        Rectangle()
+    }
+}
+
+struct SquiggleView_Previews: PreviewProvider {
+    static var previews: some View {
+        SquiggleView()
+            .padding()
+    }
+}
